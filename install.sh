611d94c30bb9e81098b2083830a57a2a7c43a11b
Shunsuke Matsuno <matsuno.shunsuke@gmail.com>
2024-12-14 02:16:18 -0500

    install image

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ TEXMFHOME=$(kpsewhich -var-value=TEXMFLOCAL)
 
 path_sty=$TEXMFHOME/tex/latex/local
 path_bst=$TEXMFHOME/bibtex/bst/local
+path_img=$TEXMFHOME/tex/latex/local/images
 
 cp ./paper/paper.sty $path_sty
 cp ./paper/aea.bst $path_bst
@@ -11,5 +12,8 @@ cp ./slide/slide.sty $path_sty
 cp ./slide/script.sty $path_sty
 
 cp ./biblatex/biblatex-aer.tex $path_sty
+
+# Copy the logo file
+cp ./images/CBS_logo.png $path_img
 
 texhash
